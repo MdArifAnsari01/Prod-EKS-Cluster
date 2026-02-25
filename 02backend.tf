50729444eed0e0516cf7c002e0e80c64e1a9d331
Arif315 <mdarifansari187@gmail.com>
2026-02-25 17:35:33 +0530

    changed the key of backend

--- 02backend.tf
+++ 02backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-terra-78978"
-    key            = "prod/terraform-statefile"
+    key            = "prod/statefiles"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "remote-backend"

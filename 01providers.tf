f3d8bde653f0b9ae21fe432e33b710c713b56285
Arif315 <mdarifansari187@gmail.com>
2026-02-27 16:21:47 +0530

    added cluster-autoscaler

--- 01providers.tf
+++ 01providers.tf
@@ -24,17 +24,17 @@ provider "aws" {
 
 #get configuration using Exec Plugins
 
-# provider "kubernetes" {
-#   # host                   = data.aws_eks_cluster.eks-cluster.endpoint
-#   host = module.eks.cluster_endpoint
-#   # cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#     command     = "aws"
-#   }
-# }
+provider "kubernetes" {
+  # host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  host = module.eks.cluster_endpoint
+  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+}
 
 
 
